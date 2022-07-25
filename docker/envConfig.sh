c7d7141ce4859da8245f5c795760ee3fb81ef75a
Shane Mc Cormack <shanemcc@gmail.com>
2022-07-26 00:36:29 +0100

    Fix envConfig.sh

--- docker/envConfig.sh
+++ docker/envConfig.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 
 CONFIGSOURCE="/usr/share/nginx/html/config.json"
-CONFIGTEMP="/tmp/config.json"
+CONFIGTEMP="${CONFIGSOURCE}.temp"
+
+echo "{}" > "${CONFIGSOURCE}"
 
 if [ "${SITENAME}" != "" ]; then
+    echo "Setting siteName: ${SITENAME}"
     cat "${CONFIGSOURCE}" | jq '.siteName |= "'"${SITENAME}"'"' > "${CONFIGTEMP}"
+    cat ${CONFIGTEMP}
+    mv "${CONFIGTEMP}" "${CONFIGSOURCE}"
 fi;
 
 if [ "${SERVERADDR}" != "" ]; then
+    echo "Setting server: ${SERVERADDR}"
     cat "${CONFIGSOURCE}" | jq '.server |= "'"${SERVERADDR}"'"' > "${CONFIGTEMP}"
+    mv "${CONFIGTEMP}" "${CONFIGSOURCE}"
 fi;
 
-cat "${CONFIGSOURCE}" | jq '.updated |= "'"$(date)"'"' > "${CONFIGTEMP}"
-
+UPDATED=$(date)
+echo "Setting updated: ${UPDATED}"
+cat "${CONFIGTEMP}" | jq '.updated |= "'"${UPDATED}"'"' > "${CONFIGTEMP}"
 mv "${CONFIGTEMP}" "${CONFIGSOURCE}"
 
 echo 'Site config is set: '
