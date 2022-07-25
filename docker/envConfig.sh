6ab2eff055a21975abf6deae976d4a0b7186ae01
Shane Mc Cormack <shanemcc@gmail.com>
2022-07-26 00:41:22 +0100

    Fix envConfig.sh

--- docker/envConfig.sh
+++ docker/envConfig.sh
@@ -3,12 +3,9 @@
 CONFIGSOURCE="/usr/share/nginx/html/config.json"
 CONFIGTEMP="${CONFIGSOURCE}.temp"
 
-echo "{}" > "${CONFIGSOURCE}"
-
 if [ "${SITENAME}" != "" ]; then
     echo "Setting siteName: ${SITENAME}"
     cat "${CONFIGSOURCE}" | jq '.siteName |= "'"${SITENAME}"'"' > "${CONFIGTEMP}"
-    cat ${CONFIGTEMP}
     mv "${CONFIGTEMP}" "${CONFIGSOURCE}"
 fi;
 
@@ -20,7 +17,7 @@ fi;
 
 UPDATED=$(date)
 echo "Setting updated: ${UPDATED}"
-cat "${CONFIGTEMP}" | jq '.updated |= "'"${UPDATED}"'"' > "${CONFIGTEMP}"
+cat "${CONFIGSOURCE}" | jq '.updated |= "'"${UPDATED}"'"' > "${CONFIGTEMP}"
 mv "${CONFIGTEMP}" "${CONFIGSOURCE}"
 
 echo 'Site config is set: '
