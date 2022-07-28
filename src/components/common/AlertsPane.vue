8d806626b22b35940fba8916684a2330a390c9ea
Shane Mc Cormack <shanemcc@gmail.com>
2022-07-28 05:08:33 +0100

    Don't use v-for on root.

--- src/components/common/AlertsPane.vue
+++ src/components/common/AlertsPane.vue
@@ -1,8 +1,10 @@
 <template>
+  <div>
     <div v-for="(alert, id) in alerts" :key="id" :class="'alert alert-dismissible alert-' + alert.type" role="alert">
         {{ alert.message }}
         <button type="button" class="btn-close" @click="$emit('removeAlert', id)" aria-label="Close"></button>
     </div>
+  </div>
 </template>
 
 <script>
