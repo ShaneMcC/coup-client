8646ed203837224c5e78a897e9ac78cd9e2f8f85
Shane Mc Cormack <shanemcc@gmail.com>
2022-08-15 09:05:02 +0100

    Fix header/footer vanishing.

--- src/views/RulesPage.vue
+++ src/views/RulesPage.vue
@@ -2,6 +2,14 @@
     <div class="container text-center">
         <h2>Rules and Gameplay</h2>
         <RulesPane></RulesPane>
+
+        <component :is="'style'">
+            footer, nav { display: none !important; }
+            body {
+                padding-top: 0 !important;
+                margin-bottom: 0 !important;
+            }
+        </component>
     </div>
 </template>
 
@@ -12,14 +20,3 @@ export default {
     components: { RulesPane }
 }
 </script>
-
-<style lang="scss">
-footer, nav {
-    display: none !important;
-}
-
-body {
-    padding-top: 0 !important;
-    margin-bottom: 0 !important;
-}
-</style>
